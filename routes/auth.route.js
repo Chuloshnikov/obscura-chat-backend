ad158b460e0a3f035ffd4cc157892bc6d6d661ff
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-03-13 13:30:34 +0200

    done

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { logIn, logOut, signUp } from "../controllers/auth.controller.js";
+import { logIn, logOut, signUp, updateProfile } from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/sign-up", signUp);
-
 authRouter.post("/sign-in", logIn);
-
 authRouter.post("/sign-out", logOut);
+
+authRouter.put("/update-profile", updateProfile);
 
 export default authRouter;
