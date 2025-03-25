f6261ea659049caaf038ca30c7c1b7d2184d9d70
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-03-25 21:16:33 +0200

    done

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ import { ORIGIN, PORT } from "./config/env.js";
 import authRoutes from "./routes/auth.route.js";
 import messagesRoutes from "./routes/messages.route.js";
 import { connectDB } from "./database/mongodb.js";
+import { app, server } from "./lib/socket.js";
 
 
-const app = express();
+app
 const port = PORT || 8081;
 
 
@@ -35,7 +36,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to Obscura chat api v2.0!');
 });
 
-const server = app.listen(port, async () => {
+server.listen(port, async () => {
     console.log(`Server running on http://localhost:${port}`);
     await connectDB();
 });
