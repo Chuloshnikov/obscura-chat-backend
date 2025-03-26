01ac97d83c974b5739ba406d685095cd87c454d3
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-03-26 21:40:58 +0200

    done

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,6 +2,8 @@ import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
+import { getReceiverSocketId, io } from "../lib/socket.js";
+
 
 export const getOtherUsers = async (req, res) => {
     try {
@@ -40,27 +42,29 @@ export const sendMessage = async (req, res) => {
         const { text, image } = req.body;
         const { id: receiverId } = req.params;
         const senderId = req.user._id;
-
+  
         let imageUrl;
         if (image) {
             // Upload base64 image to cloudinary
             const uploadResponse = await cloudinary.uploader.upload(image);
             imageUrl = uploadResponse.secure_url;
         }
-
+    
         const newMessage = new Message({
-            senderId, 
+            senderId,
             receiverId,
             text,
             image: imageUrl,
         });
-
+  
         await newMessage.save();
-
-        //real time with socket.io
-
+  
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+  
         res.status(201).json(newMessage);
-
     } catch (error) {
         console.log("Error in sendMessage controller: ", error.message);
         res.status(500).json({ error: "Internal server error" });
